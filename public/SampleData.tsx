b157a0819dafd094e6b9e079d0a41d60480dae6b
soryeongk <rlathfud2@gmail.com>
2021-07-02 20:42:38 +0900

    refator: add sample data for customizing

---
+++ public/SampleData.tsx
@@ -0,0 +1,42 @@
+import Sample from '../assets/main/PerfumeSample.svg';
+
+const SampleData: object[] = [
+  {
+    image: Sample,
+    keyword: '#sample',
+  },
+  {
+    image: Sample,
+    keyword: '#sample',
+  },
+  {
+    image: Sample,
+    keyword: '#sample',
+  },
+  {
+    image: Sample,
+    keyword: '#sample',
+  },
+  {
+    image: Sample,
+    keyword: '#sample',
+  },
+  {
+    image: Sample,
+    keyword: '#sample',
+  },
+  {
+    image: Sample,
+    keyword: '#sample',
+  },
+  {
+    image: Sample,
+    keyword: '#sample',
+  },
+  {
+    image: Sample,
+    keyword: '#sample',
+  },
+];
+
+export default SampleData;
