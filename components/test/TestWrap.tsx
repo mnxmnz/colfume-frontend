59963946e810837f2c352b0162d1b03335caba6e
mnxmnz <codi0703@naver.com>
2021-07-07 10:36:10 +0900

    fix: 테스트 컴포넌트 위치 변경

--- components/test/TestWrap.tsx
+++ components/test/TestWrap.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import styled from 'styled-components';
+import Questions from './questions/Questions';
 
 function TestWrap() {
-  return <div>TestWrap</div>;
+  return <Questions />;
 }
 
 export default TestWrap;
