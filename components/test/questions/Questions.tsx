2dada7e5fa21f53347426291cbfcc7a1f91a7975
mnxmnz <codi0703@naver.com>
2021-07-13 14:20:17 +0900

    feat: progres bar 생성

--- components/test/questions/Questions.tsx
+++ components/test/questions/Questions.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import AnswerList from './AnswerList';
+import ProgressBar from './ProgressBar';
 import QuestionList from './QuestionList';
 
 function Questions() {
@@ -8,6 +9,7 @@ function Questions() {
     <>
       <QuestionList />
       <AnswerList />
+      <ProgressBar />
     </>
   );
 }
