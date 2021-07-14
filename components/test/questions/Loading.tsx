3c5308323713740305c42542f4f79daea6976fe2
yezgoget <yejison98@gmail.com>
2021-07-15 02:12:21 +0900

    feat: modal 중앙정렬

--- components/test/questions/Loading.tsx
+++ components/test/questions/Loading.tsx
@@ -29,10 +29,10 @@ const LoadingWrap = styled.div`
 `;
 
 const IconWrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 
   img {
     animation: loadingAnimation 2s;
--- states/test.ts
+++ states/test.ts
@@ -4,3 +4,8 @@ export const questionNumberAtom = atom({
   key: 'questionNumberAtom',
   default: 0,
 });
+
+export const modalIsOpenAtom = atom({
+  key: 'modalIsOpenAtom',
+  default: false,
+});
