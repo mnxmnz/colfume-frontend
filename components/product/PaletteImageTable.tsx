18b7b412167f75b855d476e93ac7c08040d0949d
soryeongk <rlathfud2@gmail.com>
2021-07-15 19:40:37 +0900

    feat: connect server data product page

---
+++ components/product/PaletteImageTable.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import PerfumeImg from '../search/PerfumeImg';
+import { media } from '@styles/theme';
+import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
+import { paletteAtom } from '../../states/product';
+import { GetProductList } from '../../lib/api/product/getProduct';
+
+interface MoodType {
+  _id: string;
+  mood_name: string;
+}
+
+function PaletteImageTable() {
+  const keyword = useRecoilValue(paletteAtom);
+  console.log('color: ', keyword);
+  const rawData = GetProductList(keyword);
+  console.log(rawData);
+
+  return (
+    <ImageTableWrap>
+      <ImageTableBox>
+        {rawData.data &&
+          rawData.data.map((data, idx) => {
+            const moods: [string, MoodType][] = Object.entries(data.moods[0]);
+            return (
+              <PerfumeImg
+                key={data._id}
+                image={data.perfume_image}
+                name={data.perfume_name}
+                keyword={moods.map(mood => mood[1] && `#${mood[1].mood_name}`)}
+              />
+            );
+          })}
+      </ImageTableBox>
+    </ImageTableWrap>
+  );
+}
+
+const ImageTableWrap = styled.div`
+  justify-content: center;
+  padding-top: 13rem;
+  padding-bottom: 38.4rem;
+
+  ${media.custom[1300]} {
+    padding-bottom: 20rem;
+  }
+
+  ${media[768]} {
+    padding-bottom: 10rem;
+  }
+
+  ${media.mobile} {
+    padding-top: 3.2rem;
+    padding-bottom: 2.6rem;
+  }
+`;
+
+const ImageTableBox = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 3.2rem;
+  justify-content: center;
+  margin: 0 auto;
+  width: 100%;
+  max-width: 113.6rem;
+
+  ${media[1440]} {
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 3rem;
+    max-width: 84rem;
+  }
+
+  ${media[768]} {
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 2.8rem;
+    max-width: 34rem;
+  }
+
+  ${media.mobile} {
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 1.5rem;
+  }
+`;
+
+export default PaletteImageTable;
---
+++ lib/api/product/getProduct.ts
@@ -0,0 +1,16 @@
+import useSWR from 'swr';
+import { fetcher } from '../fetch';
+
+const BASEURL = `https://colfume.com/api/product`;
+export const GetProductList = props => {
+  const url = props ? `/${props}` : '/';
+  const { data, error } = useSWR(BASEURL + url, fetcher, {
+    onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+      if (error.status === 404) return;
+    },
+  });
+
+  if (error) return '[FAIL] get data';
+  if (!data) return '[FAIL] no data';
+  return data;
+};
