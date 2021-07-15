565d96a370a349492242b07d404cbd7f5382e47d
yezgoget <yejison98@gmail.com>
2021-07-16 02:26:21 +0900

    temp save footer

---
+++ components/common/MobileFooter.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Link from 'next/link';
+import { FooterIcons } from '../../assets';
+import Image from 'next/image';
+import { media } from '@styles/theme';
+import { Logo } from '../../assets';
+
+const MobileFooter = () => {
+  return (
+    <MobileBox>
+      <MaterialBox>
+        <LogoWrapper>
+          <Image src={Logo} />
+        </LogoWrapper>
+      </MaterialBox>
+    </MobileBox>
+  );
+};
+
+const MobileBox = styled.div`
+  bottom: 0;
+  margin-top: auto;
+  background: ${({ theme }) => theme.colors.gray1};
+  width: 37.5rem;
+  min-height: 25.6rem;
+`;
+const LogoWrapper = styled.div``;
+const MaterialBox = styled.div``;
+
+export default MobileFooter;
