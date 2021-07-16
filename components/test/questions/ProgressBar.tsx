9c74918115a70d6a3ae57a2dd93295870ddf41d6
mnxmnz <codi0703@naver.com>
2021-07-17 02:43:09 +0900

    feat: progress bar (animation)

--- components/test/questions/ProgressBar.tsx
+++ components/test/questions/ProgressBar.tsx
@@ -9,27 +9,47 @@ function ProgressBar() {
   const progress = useRecoilValue(questionNumberAtom);
 
   return (
-    <ProgressBarWrap>
-      <Current />
-    </ProgressBarWrap>
+    <>
+      <NumberWrap>
+        <CurrentProgress progress={progress}>{progress + 1}/7</CurrentProgress>
+      </NumberWrap>
+      <ProgressBarWrap>
+        <Current progress={progress} />
+      </ProgressBarWrap>
+    </>
   );
 }
 
-const ProgressBarWrap = styled.div`
+const NumberWrap = styled.div`
   margin: 0 auto;
   margin-top: 16.9444vh;
+  width: 63rem;
+`;
+
+const CurrentProgress = styled.div<{ progress: number }>`
+  transition: all 1s ease-in-out;
+  width: ${({ progress }) => 9 * (progress + 1) + 0.8}rem;
+  text-align: right;
+  line-height: 2.2rem;
+  font-family: Junge;
+  font-size: 1.2rem;
+  font-weight: 400;
+`;
+
+const ProgressBarWrap = styled.div`
+  margin: 0 auto;
   background-color: ${({ theme }) => theme.colors.progressBeige};
   width: 63rem;
   height: 1rem;
 `;
 
-const Current = styled.div`
+const Current = styled.div<{ progress: number }>`
   position: absolute;
-  transition: all 0.5s ease-in-out;
+  transition: all 1s ease-in-out;
   z-index: 10;
-  border-radius: 20px;
   background-color: ${({ theme }) => theme.colors.borderGray};
-  width: clac(9 * progress) rem;
+  width: ${({ progress }) => 9 * (progress + 1)}rem;
+  height: 1rem;
 `;
 
 export default ProgressBar;
