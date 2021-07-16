a3105744574a16628992e5a69e4fa1cbb4ed6fc2
mnxmnz <codi0703@naver.com>
2021-07-16 18:24:11 +0900

    fix: 서버 연결 오류 해결

--- lib/api/test/postAnswer.ts
+++ lib/api/test/postAnswer.ts
@@ -3,8 +3,8 @@ const baseURL = 'https://colfume.co.kr/api';
 
 export async function testResult(answer) {
   try {
-    const response = await axios.post(`${baseURL}/colfume/test`, answer);
-    return response;
+    const { data } = await axios.post(`${baseURL}/colfume/test`, answer);
+    return data;
   } catch (error) {
     console.error('Test Result Error', error);
   }
