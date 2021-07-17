27b9887ae96f76b347a78c753f77d4fff74a9d05
mnxmnz <codi0703@naver.com>
2021-07-17 11:26:05 +0900

    fix: 서버 API 연결 수정

---
+++ lib/api/test/getAnswer.ts
@@ -0,0 +1,15 @@
+import useSWR from 'swr';
+import { fetcher } from '../fetch';
+
+export const GetTestAnswer = colorName => {
+  const { data, error } = useSWR(`https://colfume.co.kr/api/colfume/White`, fetcher, {
+    onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+      if (error.status === 404) return;
+      if (retryCount >= 10) return;
+    },
+  });
+
+  if (error) return '[FAIL] get data';
+  if (!data) return '[FAIL] no data';
+  return data;
+};
