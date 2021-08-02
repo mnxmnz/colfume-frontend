d2774dba8b6d0bc869688fb0177a4395bc0cf96b
mnxmnz <codi0703@naver.com>
2021-08-02 17:10:15 +0900

    chore: assets 절대 경로 설정 및 샘플 파일 삭제

--- assets/index.ts
+++ assets/index.ts
@@ -1,3 +1,14 @@
+///// main assets
+export { default as ArrowLeftHovered } from './main/ArrowLeftHovered.svg';
+export { default as ArrowLeftUnHovered } from './main/ArrowLeftUnHovered.svg';
+export { default as ArrowRightHovered } from './main/ArrowRightHovered.svg';
+export { default as ArrowRightUnHovered } from './main/ArrowRightUnHovered.svg';
+export { default as BannerImg } from './main/BannerImg.svg';
+export { default as Contour } from './main/Contour.svg';
+export { default as FooterIcons } from './main/FooterIcons.svg';
+export { default as FooterLogo } from './main/FooterLogo.svg';
+export { default as InstagramIcon } from './main/InstagramIcon.svg';
+export { default as Logo } from './main/Logo.svg';
 export { default as Palette01Red } from './main/Palette01Red.svg';
 export { default as Palette02Orange } from './main/Palette02Orange.svg';
 export { default as Palette03Vanilla } from './main/Palette03Vanilla.svg';
@@ -6,34 +17,32 @@ export { default as Palette05Sky } from './main/Palette05Sky.svg';
 export { default as Palette06Purple } from './main/Palette06Purple.svg';
 export { default as Palette07Pink } from './main/Palette07Pink.svg';
 export { default as Palette08White } from './main/Palette08White.svg';
-export { default as ArrowLeftUnHovered } from './main/ArrowLeftUnHovered.svg';
-export { default as ArrowLeftHovered } from './main/ArrowLeftHovered.svg';
-export { default as ArrowRightUnHovered } from './main/ArrowRightUnHovered.svg';
-export { default as ArrowRightHovered } from './main/ArrowRightHovered.svg';
-export { default as BannerImg } from './main/BannerImg.svg';
-export { default as Contour } from './main/Contour.svg';
-export { default as FooterIcons } from './main/FooterIcons.svg';
-export { default as SearchIcon } from './search/SearchIcon.svg';
-export { default as SearchArrowOn } from './search/SearchArrowOn.svg';
-export { default as SearchArrowOff } from './search/SearchArrowOff.svg';
-export { default as SearchCheckboxOn } from './search/SearchCheckboxOn.svg';
-export { default as SearchCheckboxOff } from './search/SearchCheckboxOff.svg';
-export { default as SearchCheckboxHover } from './search/SearchCheckboxHover.svg';
-export { default as Logo } from './main/Logo.svg';
-export { default as LoadingIcon } from './test/LoadingIcon.svg';
-export { default as FilterRed } from './product/FilterRed.svg';
-export { default as FilterOrange } from './product/FilterOrange.svg';
-export { default as FilterVanilla } from './product/FilterVanilla.svg';
+///// product assets
 export { default as FilterGreen } from './product/FilterGreen.svg';
-export { default as FilterSky } from './product/FilterSky.svg';
-export { default as FilterPurple } from './product/FilterPurple.svg';
+export { default as FilterOrange } from './product/FilterOrange.svg';
 export { default as FilterPink } from './product/FilterPink.svg';
+export { default as FilterPurple } from './product/FilterPurple.svg';
+export { default as FilterRed } from './product/FilterRed.svg';
+export { default as FilterSky } from './product/FilterSky.svg';
+export { default as FilterVanilla } from './product/FilterVanilla.svg';
 export { default as FilterWhite } from './product/FilterWhite.svg';
-export { default as ResultOrange } from './result/ResultOrange.svg';
-export { default as Green } from './result/Green.svg';
+///// productDetail assets
 export { default as ButtonBackDefault } from './productDetail/ButtonBackDefault.svg';
 export { default as IconMainCol } from './productDetail/IconMainCol.svg';
+export { default as IconSubColGreen } from './productDetail/IconSubColGreen.svg';
 export { default as IconSubColRed } from './productDetail/IconSubColRed.svg';
 export { default as IconSubColVanilla } from './productDetail/IconSubColVanilla.svg';
-export { default as IconSubColGreen } from './productDetail/IconSubColGreen.svg';
+///// questions assets
+export { default as LoadingIcon } from './questions/LoadingIcon.svg';
+///// result assets
+export { default as Green } from './result/Green.svg';
 export { default as Line } from './result/Line.svg';
+export { default as ResultOrange } from './result/ResultOrange.svg';
+///// search assets
+export { default as PerfumeImg } from './search/PerfumeImg.svg';
+export { default as SearchArrowOff } from './search/SearchArrowOff.svg';
+export { default as SearchArrowOn } from './search/SearchArrowOn.svg';
+export { default as SearchCheckboxHover } from './search/SearchCheckboxHover.svg';
+export { default as SearchCheckboxOff } from './search/SearchCheckboxOff.svg';
+export { default as SearchCheckboxOn } from './search/SearchCheckboxOn.svg';
+export { default as SearchIcon } from './search/SearchIcon.svg';
