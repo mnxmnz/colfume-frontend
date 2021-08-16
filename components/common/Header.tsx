339b6241831848df9aa5233e4b1df5834e50ec4e
kimyeongseo <oseo0202@naver.com>
2021-08-17 00:11:53 +0900

    feat:Header IsMobile 적용

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -5,44 +5,56 @@ import { media } from '@styles/theme';
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
+import sizeMe from 'react-sizeme';
+import { ISizeProps } from 'types/main';
+import MobileHeader from './MobileHeader';
 
 interface IStyledProps {
   current: boolean;
 }
 
-const Header = () => {
+const Header = (props: ISizeProps) => {
   const router = useRouter();
   const currentPath = router.pathname;
 
+  const { width } = props.size;
+  const isMobile = width <= 768 ? true : false;
+
   return (
-    <Positioner>
-      <WhiteBackground>
-        <Link href="/" passHref>
-          <LogoWrap current={currentPath === '/'}>
-            <Image src={Logo} id="Logo" alt="" />
-          </LogoWrap>
-        </Link>
-        <Layout>
-          <Category>
-            <Link href="/product" passHref>
-              <CategoryBtn id="Product" current={currentPath === '/product'}>
-                Product
-              </CategoryBtn>
+    <>
+      {isMobile ? (
+        <MobileHeader />
+      ) : (
+        <Positioner>
+          <WhiteBackground>
+            <Link href="/" passHref>
+              <LogoWrap current={currentPath === '/'}>
+                <Image src={Logo} id="Logo" alt="" />
+              </LogoWrap>
             </Link>
-            <Link href="/test" passHref>
-              <CategoryBtn id="ColorTest" current={currentPath === '/test'}>
-                Color Test
-              </CategoryBtn>
-            </Link>
-            <Link href="/search" passHref>
-              <CategoryBtn id="Search" current={currentPath === '/search'}>
-                Search
-              </CategoryBtn>
-            </Link>
-          </Category>
-        </Layout>
-      </WhiteBackground>
-    </Positioner>
+            <Layout>
+              <Category>
+                <Link href="/product" passHref>
+                  <CategoryBtn id="Product" current={currentPath === '/product'}>
+                    Product
+                  </CategoryBtn>
+                </Link>
+                <Link href="/test" passHref>
+                  <CategoryBtn id="ColorTest" current={currentPath === '/test'}>
+                    Color Test
+                  </CategoryBtn>
+                </Link>
+                <Link href="/search" passHref>
+                  <CategoryBtn id="Search" current={currentPath === '/search'}>
+                    Search
+                  </CategoryBtn>
+                </Link>
+              </Category>
+            </Layout>
+          </WhiteBackground>
+        </Positioner>
+      )}
+    </>
   );
 };
 
@@ -59,11 +71,6 @@ const WhiteBackground = styled.div`
   background: ${({ theme }) => theme.colors.white};
   width: 100vw;
   height: 7.2rem;
-
-  ${media.mobile} {
-    border-bottom: 0.1rem solid ${({ theme }) => theme.colors.gray2};
-    height: 4.5rem;
-  }
 `;
 
 const Layout = styled.div`
@@ -77,14 +84,6 @@ const Layout = styled.div`
   ${media[1440]} {
     max-width: 84rem;
   }
-
-  ${media[768]} {
-    max-width: 35rem;
-  }
-
-  ${media.mobile} {
-    height: 4.5rem;
-  }
 `;
 
 const LogoWrap = styled.div<IStyledProps>`
@@ -97,17 +96,6 @@ const LogoWrap = styled.div<IStyledProps>`
   ${media[1440]} {
     margin-left: 6rem;
   }
-
-  ${media[768]} {
-    top: 2.2rem;
-    margin-left: 3rem;
-    width: 10rem;
-  }
-
-  ${media.mobile} {
-    top: 1.1rem;
-    width: 8rem;
-  }
 `;
 
 const Category = styled.div`
@@ -118,48 +106,16 @@ const Category = styled.div`
     margin-right: 4rem;
   }
 
-  ${media[768]} {
-    margin-left: 5rem;
-  }
-
-  ${media.mobile} {
-    margin-left: 12rem;
-  }
-
   #Product {
     width: 10rem;
-
-    ${media[768]} {
-      width: 8.5rem;
-    }
-
-    ${media.mobile} {
-      width: 5.5rem;
-    }
   }
 
   #ColorTest {
     width: 12.7rem;
-
-    ${media[768]} {
-      width: 11.5rem;
-    }
-
-    ${media.mobile} {
-      width: 6.8rem;
-    }
   }
 
   #Search {
     width: 8.8rem;
-
-    ${media[768]} {
-      width: 7.5rem;
-    }
-
-    ${media.mobile} {
-      width: 4.8rem;
-    }
   }
 `;
 
@@ -171,17 +127,6 @@ const CategoryBtn = styled.span<IStyledProps>`
   line-height: 2.674rem;
   font-family: 'Junge';
   font-size: 2.2rem;
-
-  ${media[768]} {
-    margin: 0 1.3rem;
-    font-size: 1.8rem;
-  }
-
-  ${media.mobile} {
-    margin: 0 1rem;
-    line-height: 1.458rem;
-    font-size: 1.2rem;
-  }
 `;
 
-export default Header;
+export default sizeMe({ monitorHeight: true })(Header);
