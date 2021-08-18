139e4120b482a4a1eedd44bd0e449c5d498e9d7a
yezi <yejison98@gmail.com>
2021-08-18 16:14:49 +0900

    chore: mediaquery breakpoint

--- styles/theme.ts
+++ styles/theme.ts
@@ -13,11 +13,13 @@ export const theme: DefaultTheme = {
 };
 
 const customMediaQuery = (maxWidth: number): string => `@media (max-width: ${maxWidth}px)`;
+const mobileMediaQuery = (minWidth: number, maxWidth: number): string =>
+  `@media (min-width: ${minWidth}px) and (max-width: ${maxWidth})`;
 
 export const media = {
   custom: customMediaQuery,
   1440: customMediaQuery(1440),
   1100: customMediaQuery(1100),
   768: customMediaQuery(768),
-  mobile: customMediaQuery(375),
+  mobile: mobileMediaQuery(375, 420),
 };
