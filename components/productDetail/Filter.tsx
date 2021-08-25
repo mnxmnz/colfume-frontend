2497a00221ed31a5f02d221fd69a4fc8ee6df94b
soryeongk <rlathfud2@gmail.com>
2021-08-25 12:55:23 +0900

    chore: del unused tag

--- components/productDetail/Filter.tsx
+++ components/productDetail/Filter.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { media } from '@styles/theme';
-import Image from 'next/image';
 
 function Filter({ moods, styles, colors }) {
   return (
